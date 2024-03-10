81052a5d86bf5c4aeaa911b51b74568249990883
Jose Pedro Silva <josepedro88@live.com.pt>
2024-03-10 09:04:07 +0100

    Refactor db.js to export a new instance of sqlite3.Database

--- db.js
+++ db.js
@@ -1,21 +1,4 @@
+// Inside db.js
 const sqlite3 = require('sqlite3').verbose();
+module.exports = new sqlite3.Database('db.sqlite');
 
-const db = new sqlite3.Database('db.sqlite', (err) => {
-    if (err) {
-        console.error(err.message);
-    } else {
-        db.run(`CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            email TEXT
-        )`, (err) => {
-            if (err) {
-                console.error(err.message, 'Error creating the users table. Table may already exist.');
-            }
-        });
-        console.log('Connected to the SQLite database.');
-    }
-});
-
-
-module.exports = db;
