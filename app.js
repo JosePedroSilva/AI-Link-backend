5ce2f50295302a01398c249caca8d8f37af0cb7d
Jose Pedro Silva <josepedro88@live.com.pt>
2024-03-21 08:27:37 +0100

    Remove unused code and export app module

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require('express')
 const cors = require('cors')
-const usersRoutes = require('./routes/users');
 const conversationsRoutes = require('./routes/conversations');
 const app = express()
-const port = 3001
 
 app.use(cors())
 app.use(express.json());
 
 app.use('/conversations', conversationsRoutes);
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+module.exports = app;
